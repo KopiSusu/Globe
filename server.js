19f32901a508a0f4f11a29c0c4f2af3a54e5f756
Xianny <xianny@gmail.com>
2014-11-13 19:59:49 -0800

    basic web server running

---
+++ server.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log("a user connected");
+
+  // socket.on('chat message', function(msg) {
+  //   io.emit('chat message', msg);
+  // });
+  // socket.on('disconnect', function() {
+  //   console.log('user disconnected');
+  // });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+})
